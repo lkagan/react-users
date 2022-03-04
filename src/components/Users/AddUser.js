604dcc0a52622534756e0e717a4dbbe85f718b83
Larry Kagan <larry@superiocity.com>
2022-03-04 10:24:00 -0500

    Set input handlers for user form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,32 @@
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from "../UI/Button";
+import {useState} from "react";
 
 const AddUser = props => {
+    const [enteredUser, setEnteredUser] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUser, enteredAge);
+    };
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUser(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     };
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor={"username"}>Username</label>
-                <input id="username" type="text"/>
+                <input id="username" type="text" onChange={usernameChangeHandler}/>
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number"/>
+                <input id="age" type="number" onChange={ageChangeHandler}/>
                 <Button type={"submit"}>Add User</Button>
             </form>
         </Card>
